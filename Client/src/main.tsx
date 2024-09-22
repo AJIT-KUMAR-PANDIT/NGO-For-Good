be15563ebf639a86696034ae2701c735a2a27a9d
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-09-22 19:50:39 +0530

    update

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
